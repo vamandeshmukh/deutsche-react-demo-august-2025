8f92d29cc4a049f662b181c329dc68d6ac510886
Vaman Deshmukh <vamandeshmukh@gmail.com>
2025-08-04 14:53:45 +0530

    done

--- src/components/learning/Child.jsx
+++ src/components/learning/Child.jsx
@@ -3,11 +3,18 @@ const Child = (props) => {
 
     const childData = 'Monu';
 
+    const sendDataToParent = () => {
+        console.log(childData);
+        props.childDataToParent(childData);
+    };
+
+
     return (
         <>
             <p>Child Component</p>
             <p>Child data in child component: {childData}</p>
             <p>Parent data in child component: {props.parentDataToChild}</p>
+            <button onClick={sendDataToParent}>Send data to parent</button>
         </>
     );
 
--- src/components/learning/Parent.jsx
+++ src/components/learning/Parent.jsx
@@ -1,4 +1,3 @@
-import { useSearchParams } from "react-router-dom";
 import Child from "./Child";
 import { useState } from "react";
 
@@ -7,12 +6,17 @@ const Parent = () => {
     const parentData = 'Sonu';
     const [childDataInParent, setChildDataInParent] = useState('');
 
+    const getDataFromChild = (data) => {
+        console.log(data);
+        setChildDataInParent(data);
+    };
+
     return (
         <>
             <p>Parent Component</p>
             <p>Parent data in parent component: {parentData} </p>
             <p>Child data in parent component: {childDataInParent}</p>
-            <Child parentDataToChild={parentData} />
+            <Child parentDataToChild={parentData} childDataToParent={getDataFromChild} />
         </>
     );
 
